559a74790b7caf8e2e1a5e6757c8525228605196
unknown <ikramkouibaati@gmail.com>
2024-07-15 15:06:36 +0200

    Navbar and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 JIRO by Annabelle&Ikram . All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
